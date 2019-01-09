9b59482e893c4362cb1698832924e88d3043a89e
HairyCap <ooooluffy.n.a@gmail.com>
2019-01-10 06:08:07 +0800

    added with mongoose branch

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
 
 const app = express();
 const server = new ApolloServer(schema);
 server.applyMiddleware({ app });
+
+mongoose.connect(
+  "mongodb://connection srting",
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to DB");
+});
 
 const PORT = process.env.post || 4002;
 app.listen(PORT, () => {
